546678d9938b540f245503ae0d7739069f62b6ad
Cornel <CorneliuMungiu@github.com>
2022-11-04 19:10:47 +0200

    the vector structure was added

--- thread_arguments.h
+++ thread_arguments.h
@@ -13,18 +13,25 @@
 #define MAPPER 0
 #define REDUCER 1
 
+typedef struct vector{
+    int *vec;
+    int size;
+}Vector;
+
 typedef struct thread_arguments{
     int id;
     int type;
     FILE *input_file;
-    int **power_arr;
+    Vector* power_arr;
     int **power_table;
     int *nr_mapper;
     int *nr_reducer;
+    char* input_file_name;
     pthread_mutex_t *mutex;
     pthread_barrier_t *barrier;
 }Thread_arguments;
 
 void *f(void* threads_arg);
+void get_index_from_power_table(int **power_table, int nr_reducer, int number, Vector *res);
 
 #endif
